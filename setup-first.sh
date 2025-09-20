8737aa6d6ff8e68f04f0440701c9733e8112c0fe
DNS <37093+DNS@users.noreply.github.com>
2025-09-21 00:14:45 +0700

    -

---
+++ setup-first.sh
@@ -0,0 +1,35 @@
+﻿#!/usr/bin/env sh
+
+# MUST BE RUN AS ROOT
+
+# enable su
+#passwd -d root
+#passwd -d dns
+
+sudo su
+
+# add 3rd party software repo
+#apt-add-repository -y contrib non-free
+
+# install powershell
+apt update
+apt install -y wget
+pwsh_filename=$( curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest | grep -iPo -m1 '(?i)name"\: "\K.+?amd64\.deb' )
+pwsh_dl=$( curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest | grep -iPo -m 1 '(?i)"browser_download_url"\: "\K.+?amd64\.deb' )
+wget -cnd $pwsh_dl
+dpkg -i $pwsh_filename
+apt install -f
+#rm $pwsh_filename
+
+# Change default shell
+#chsh -s /usr/bin/pwsh root
+#chsh -s /usr/bin/pwsh dns
+touch /root/.hushlogin
+
+
+pwsh -Command 'Set-PSReadLineOption -PredictionViewStyle ListView'
+
+
+# second stage installer
+pwsh ./setup-second.ps1
+
---
+++ setup-second.ps1
@@ -0,0 +1,17 @@
+﻿Get-Content packages-selective.txt | %{ apt install -y $_ }
+
+Get-Content packages-github.txt | %{ Invoke-RestMethod -Uri $_ } | %{ $_.assets.browser_download_url -match '\.AppImage\Z' } | %{ wget -cnd $_ }
+
+<#
+Get-Content packages-html_pages.txt |
+	%{ Invoke-WebRequest -Uri $_ } |
+	%{ $_.Links } |
+	Select-Object href |
+	%{
+		if ($_.href -match 'Debian.+?amd64' -or $_.href -match '\.deb\Z') {
+			wget -cnd $_.href;
+			break;
+		}
+	}
+#>
+
